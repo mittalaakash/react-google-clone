af1cb9558d10503d806f67c8f81320c7cdfe6b51
mittalaakash <mit.ash.619@gmail.com>
2021-08-12 19:37:39 +0530

    searchbox

---
+++ src/components/SearchBox.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './Search.css';
+import MicIcon from '@material-ui/icons/Mic';
+import SearchIcon from '@material-ui/icons/Search';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
+function SearchBox({ hideButtons = false }) {
+  const [input, setInput] = useState('');
+  const history = useHistory();
+
+  const search = e => {
+    e.preventDefault();
+    console.log('seerached', input);
+    history.push('./search'); //redirects to the search page
+  };
+
+  return (
+    <form className='search'>
+      <div className='search__input'>
+        <SearchIcon className='search__inputIcon' />
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <MicIcon />
+      </div>
+      {!hideButtons ? (
+        <div className='search__buttons'>
+          <Button type='submit' onClick={search} variant='outlined'>
+            Google Search
+          </Button>
+          <Button variant='outlined'>I'm Feeling Lucky</Button>
+        </div>
+      ) : (
+        <div className='search__buttons'>
+          <div className='search__buttonsHidden'>
+            <Button type='submit' onClick={search} variant='outlined'>
+              Google Search
+            </Button>
+            <Button variant='outlined'>I'm Feeling Lucky</Button>
+          </div>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default SearchBox;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import AppsIcon from '@material-ui/icons/Apps';
 
 import './Home.css';
 import { Avatar } from '@material-ui/core';
+import SearchBox from '../components/SearchBox';
 
 function Home() {
   return (
@@ -12,18 +13,12 @@ function Home() {
         <div className='home__headerLeft'>
           <Link to='/about'>About</Link>
           <Link to='/store'>Store</Link>
-          {/* link */}
-          {/* link */}
         </div>
         <div className='home__headerRight'>
           <Link to='/gmail'>Gmail</Link>
           <Link to='/images'>Images</Link>
           <AppsIcon />
           <Avatar />
-          {/* link */}
-          {/* link */}
-          {/* icon */}
-          {/* avatar */}
         </div>
       </div>
 
@@ -32,7 +27,9 @@ function Home() {
           src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
           alt='Google logo'
         />
-        <div className='home__inputContainer'>{/* Search */}</div>
+        <div className='home__inputContainer'>
+          <SearchBox hideButtons />
+        </div>
       </div>
     </div>
   );
